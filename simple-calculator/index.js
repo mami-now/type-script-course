4b70b1e1887254411403950daa560a3c1bc87b1c
mamijose <mmammyj@gmail.com>
2023-08-14 11:24:58 +0200

    a simple calculator using js and type-script

---
+++ simple-calculator/index.js
@@ -0,0 +1,19 @@
+// Using typeof
+
+const button = document.querySelector('.button')
+const firstInput = document.querySelector('#first-input')
+const secondInput = document.querySelector('#second-input')
+const screen = document.querySelector('.screen')
+ 
+function addNumbers(a,b) {
+        if(typeof a=='number' && typeof b=='number'){
+          screen.innerHTML= a+b;
+        }
+        else{
+          screen.innerHTML= parseInt(a) +  parseInt(b);
+        }
+ 
+}
+ 
+button.addEventListener('click', () => addNumbers(firstInput.value, secondInput.value))
+
