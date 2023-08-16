3a2034d7592b1305e1ceb50533846ee5a538e627
mamijose <mmammyj@gmail.com>
2023-08-16 12:00:26 +0200

    type-script kata 1 of returning odd or even number

--- .vscode/numberTypesMiniChallenge/index.ts
+++ .vscode/numberTypesMiniChallenge/index.ts
@@ -1,6 +1,21 @@
 // Number Types mini-challenge
 // Write a function that will only accept numbers and attend to 
 // all TypeScript weakness flags.
+
+/**
+    // String Types mini-challenge
+// Write a function that will display the most recent reviewers name next to the review total,
+// making sure to assign a type to the parameter, to prevent unwanted behaviour.
+// : string
+ *
+3) // Boolean Types mini-challenge
+// if the last reviewer is a loyalty User, can you add a star to the end of their name?
+// please do so in the existing function, and make sure to declare what type of 
+// parameters the function takes.
+// : boolean
+
+
+ */
 const reviewTotalDisplay = document.querySelector('#reviews')
 
 const reviews = [
@@ -23,3 +38,18 @@ const reviews = [
         date: '27-03-2021'
     },
 ]
+// Solution
+function showReviewTotal (value : number, reviewer: string) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer
+}
+
+showReviewTotal(reviews.length, reviews[0].name)
+
+
+// Solution 3
+function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
--- simple-calculator/index.ts
+++ simple-calculator/index.ts
@@ -15,14 +15,17 @@ function addNumbers(a: number,b: number) {
  
 button.addEventListener('click', () => addNumbers(parseInt(firstInput.value), parseInt(secondInput.value)))
 */
-const button = document.querySelector('.button') as Element
-const firstInput = document.querySelector('#first-input') as HTMLInputElement
-const secondInput = document.querySelector('#second-input') as HTMLInputElement
-export const screen = document.querySelector('.screen') as Element
+// const button = document.querySelector('.button') as Element
+// const firstInput = document.querySelector('#first-input') as HTMLInputElement
+// const secondInput = document.querySelector('#second-input') as HTMLInputElement
+// export const screen = document.querySelector('.screen') as Element
  
-function addNumbers(a: number,b: number) {
-     screen.innerHTML = (a + b).toString() 
-}
+// function addNumbers(a: number,b: number) {
+//      screen.innerHTML = (a + b).toString() 
+// }
  
-button.addEventListener('click', () => 
-    addNumbers(parseInt(firstInput.value), parseInt(secondInput.value)))
+// button.addEventListener('click', () => 
+//     addNumbers(parseInt(firstInput.value), parseInt(secondInput.value)))
+const names:  string[] = ["Dylan"];
+names.push("Jack"); // Error: Property 'push' does not exist on type 'readonly string[]'.
+// try removing the readonly modifier and see if it works?
